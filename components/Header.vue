fa7f7c14c6278ff6f73e1a1c3a4bbca268b0f833
nAa666 <naa666@MacBook.local>
2023-10-02 23:15:31 +0300

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li>
+        <NuxtLink active-class="active" class="link" to="/">
+          Home
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink active-class="active" class="link" to="/products">
+          Products
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink active-class="active" class="link" to="/about">
+          About
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+  nav{
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: black;
+    z-index: 9;
+  }
+  ul{
+    list-style: none;
+    padding: 20px 0;
+    margin: 0;
+    display: flex;
+    align-items: center;
+  }
+  ul li {
+    margin: 0 10px;
+    padding: 5px;
+  }
+  ul li a{
+    font-size: 18px;
+    color: white;
+    text-decoration: none;
+  }
+  ul li a.active{
+    color: green;
+  }
+  ul li a:hover{
+    opacity: .8;
+  }
+</style>
---
+++ pages/products.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Products page</h1>
+    <div class="products">
+      <article v-for="product in products.products">
+        <div class="art-image">
+          <img :src="product.thumbnail" alt="">
+        </div>
+        <p>{{product.title}}</p>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const { data: products } = await useFetch('https://dummyjson.com/products', {});
+</script>
+
+<style>
+  .products{
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  .products article{
+    width: 32%;
+    margin: 15px 0;
+  }
+  .products article .art-image{
+    height: 255px;
+    position: relative;
+    overflow: hidden;
+  }
+  .products article .art-image img{
+    object-fit: fill;
+  }
+</style>
